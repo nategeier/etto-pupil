966e6cafd551d1a65967ad122661f887c2fc839f
Randall A. Gordon <randall@mindkitchenmedia.com>
2013-12-05 15:35:26 -0800

    Fix noise from invalid tests

--- test/spec/controllers/Sessions.js
+++ test/spec/controllers/Sessions.js
@@ -3,7 +3,7 @@
 describe('Controller: SessionsCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('engoPupilApp'));
+  beforeEach(module('engoPupil'));
 
   var SessionsCtrl,
     scope;
@@ -16,7 +16,7 @@ describe('Controller: SessionsCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
+  //it('should attach a list of awesomeThings to the scope', function () {
+    //expect(scope.awesomeThings.length).toBe(3);
+  //});
 });
--- test/spec/directives/engoRedirectTo.js
+++ test/spec/directives/engoRedirectTo.js
@@ -3,7 +3,7 @@
 describe('Directive: engoRedirectTo', function () {
 
   // load the directive's module
-  beforeEach(module('engoPupilApp'));
+  beforeEach(module('engoPupil'));
 
   var element,
     scope;
@@ -12,9 +12,9 @@ describe('Directive: engoRedirectTo', function () {
     scope = $rootScope.$new();
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<engo-redirect-to></engo-redirect-to>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the engoRedirectTo directive');
-  }));
+  //it('should make hidden element visible', inject(function ($compile) {
+    //element = angular.element('<engo-redirect-to></engo-redirect-to>');
+    //element = $compile(element)(scope);
+    //expect(element.text()).toBe('this is the engoRedirectTo directive');
+  //}));
 });
