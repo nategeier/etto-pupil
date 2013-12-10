4b8ed0344995ea54eedba5ab284d952f879d4142
Randall A. Gordon <randall@mindkitchenmedia.com>
2013-12-10 12:40:04 -0800

    Update test to pass against loaded template

--- test/spec/directives/notLoggedInNav.js
+++ test/spec/directives/notLoggedInNav.js
@@ -1,15 +1,17 @@
-'use strict';
+"use strict";
 
-describe('Directive: notLoggedInNav', function () {
+describe( "Directive: notLoggedInNav", function() {
+  var element, scope;
 
-  // load the directive's module
-  beforeEach(module('engoPupil'));
-
-  var element,
-    scope;
-
-  beforeEach(inject(function ($rootScope) {
+  beforeEach( module( "engoPupil", "views/directives/engoNotLoggedInNav.html" ) );
+  beforeEach(inject(function ($rootScope, $compile) {
     scope = $rootScope.$new();
+    element = angular.element("<engo-not-logged-in-nav></engo-not-logged-in-nav>");
+    element = $compile(element)(scope);
   }));
 
+  it("should make hidden element visible", function() {
+    scope.$digest();
+    expect(element.text()).toBe("Toggle navigationLogin");
+  });
 });
