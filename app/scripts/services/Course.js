2b93530be11021baa07be0bfce2db5ccd4ce0fc6
Randall A. Gordon <randall@mindkitchenmedia.com>
2014-01-05 00:12:17 -0800

    Inject empty course when switching to CourseEdit for new Courses

--- app/scripts/services/Course.js
+++ app/scripts/services/Course.js
@@ -1,6 +1,20 @@
 "use strict";
 
+/**
+ * @module Course
+ * Course.js
+ * ============================================================================
+ *
+ * Creates AngularJS services "Course" and "CourseLoader".
+ *
+ */
+
 angular.module("ettoPupil")
+/**
+ * Course
+ *
+ * @param {course} newCourse Optional new course information.
+ */
   .factory("Course", ["$resource",
     function ($resource) {
       return $resource("/api/v1/module", {}, {
@@ -10,6 +24,11 @@ angular.module("ettoPupil")
       });
     }
   ])
+/**
+ * CourseLoader
+ *
+ * @param {Object} findParams Optional keys to pass to find
+ */
   .factory("CourseLoader", ["Course", "$route", "$q",
     function (Course, $route, $q) {
       return function () {
@@ -19,7 +38,25 @@ angular.module("ettoPupil")
         }, function (course) {
           delay.resolve(course);
         }, function () {
-          delay.reject("Unable to fetch course " + $route.current.params.courseId);
+          // Are we in edit mode? If so, send back a new course.
+          if ($route.current.controller === "CourseEditCtrl") {
+            var newCourse = new Course({
+              id: $route.current.params.courseId,
+              blocks: [{
+                type: "title",
+                data: {
+                  title: "This is a new course!",
+                  subtitle: "Fill it with delicious educational goodness..."
+                }
+              }]
+            });
+            // TODO: This will end up creating a course for potentially any random ID...
+            // Which is bad. Very bad. Instead do something on the "course-save" event.
+            newCourse.$save();
+            delay.resolve(newCourse);
+          } else {
+            delay.reject("Unable to fetch course " + $route.current.params.courseId);
+          }
         });
         return delay.promise;
       };
