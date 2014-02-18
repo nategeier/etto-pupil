86fc4da24e01d636fc941bfda9baf2772722370a
Nate <nate@interactivebalance.com>
2014-02-18 10:30:51 -0800

    karma update

--- karma.conf.js
+++ karma.conf.js
@@ -1,19 +1,20 @@
 // Karma configuration
 // http://karma-runner.github.io/0.10/config/configuration-file.html
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
     // base path, that will be used to resolve files and exclude
     basePath: "",
 
     // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: [ "jasmine" ],
+    frameworks: ["jasmine"],
 
     // list of files / patterns to load in the browser
     files: [
       "app/bower_components/jquery/jquery.min.js",
       "app/bower_components/angular/angular.js",
       "app/bower_components/angular-route/angular-route.js",
+      "app/bower_components/angular-animate/angular-animate.js",
       "app/bower_components/angular-resource/angular-resource.js",
       "app/bower_components/angular-bootstrap/ui-bootstrap.js",
       "app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js",
@@ -34,8 +35,8 @@ module.exports = function(config) {
 
     preprocessors: {
       "test/**/*.coffee": "coffee",
-      "app/views/**/*.jade": [ "ng-html2js" ],
-      "*.js": [ "coverage" ]
+      "app/views/**/*.jade": ["ng-html2js"],
+      "*.js": ["coverage"]
     },
 
     ngHtml2JsPreprocessor: {
@@ -50,10 +51,8 @@ module.exports = function(config) {
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
-
 
     // Start these browsers, currently available:
     // - Chrome
@@ -63,14 +62,12 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: [ "PhantomJS" /*, "Chrome", "Firefox"*/ ],
-
+    browsers: ["PhantomJS" /*, "Chrome", "Firefox"*/ ],
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
     singleRun: false,
 
-    reporters: [ "progress" ]
+    reporters: ["progress"]
   });
 };
-
