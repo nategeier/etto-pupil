5eb8e224451700a96b7225fa0727a72946446951
Nate <nate@interactivebalance.com>
2014-05-05 15:13:09 -0700

    production ready app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -86,7 +86,7 @@ angular.module("ettoPupil", ["ngRoute", "ngResource", "ngAnimate", "ngTouch", "u
         });
     }
   ]).run(function ($rootScope, editableOptions) {
-    var environment = "development";
+    var environment = "production";
 
     // Production
     if (environment === "production") {
