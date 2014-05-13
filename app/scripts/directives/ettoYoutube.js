f660c9a56de461676e3c117c5c8ee9de13cf6c3a
Nate <nate@interactivebalance.com>
2014-05-13 11:24:51 -0700

    youtube for https

--- app/scripts/directives/ettoYoutube.js
+++ app/scripts/directives/ettoYoutube.js
@@ -11,7 +11,7 @@ angular.module("ettoPupil")
         },
         template: "<div class='video-container'><iframe width='100%' height='100%' src='{{ ytUrl }}' frameborder='0' allowfullscreen></iframe></div>",
         link: function (scope, element, attrs) {
-          scope.ytUrl = $sce.trustAsResourceUrl("http://www.youtube.com/embed/" + scope.code);
+          scope.ytUrl = $sce.trustAsResourceUrl("//www.youtube.com/embed/" + scope.code);
         }
 
       };
