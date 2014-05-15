a166e243fea9eb27adeeb64b981f6f2d1b8c9731
Nate <nate@interactivebalance.com>
2014-05-15 14:01:20 -0700

    delete evalaute from block quiz

--- app/scripts/directives/course/ettoBlockQuiz.js
+++ app/scripts/directives/course/ettoBlockQuiz.js
@@ -28,36 +28,6 @@ angular.module("ettoPupil")
           $scope.deleteQuestion = function (questions, index) {
             BlockQuiz.deleteQuestion(questions, index);
           };
-
-          $scope.checkAnswer = function (correct, index) {
-
-            $(incorrectQuestions[qIndex]).addClass("chosen");
-            var id = "#question" + index;
-            var qIndex = _.indexOf(incorrectQuestions, id);
-
-            if (correct === false) {
-              if (qIndex === -1) {
-                incorrectQuestions.push(id);
-              }
-            } else {
-              if (qIndex !== -1) {
-                $(incorrectQuestions[qIndex]).removeClass("incorrect");
-                incorrectQuestions.splice(qIndex, 1);
-              }
-            }
-          };
-
-          $scope.evaluate = function () {
-            var totalQuestions;
-
-            for (var i = 0; i < incorrectQuestions.length; i++) {
-              $(incorrectQuestions[i]).addClass("incorrect");
-            }
-            if (!incorrectQuestions[0]) {
-              $scope.nextBlock();
-            }
-
-          };
         },
 
         link: function postLink(scope, element, attrs) {}
