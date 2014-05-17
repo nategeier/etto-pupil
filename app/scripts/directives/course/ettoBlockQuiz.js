06790c7f94607c115df34a34c1d1b11e3999768e
Nate <nate@interactivebalance.com>
2014-05-16 17:14:59 -0700

    merged block quiz to one

--- app/scripts/directives/course/ettoBlockQuiz.js
+++ app/scripts/directives/course/ettoBlockQuiz.js
@@ -28,6 +28,46 @@ angular.module("ettoPupil")
           $scope.deleteQuestion = function (questions, index) {
             BlockQuiz.deleteQuestion(questions, index);
           };
+
+          var incorrectQuestions = [];
+          $scope.alphabet = BlockQuiz.alphabet;
+          var correctNum = 0;
+
+          $scope.checkAnswer = function (answers, answer, index) {
+            for (var i = 0; i < answers.length; i++) {
+              if (answer === answers[i]) {
+                answers[i].selected = true;
+              } else {
+                answers[i].selected = false;
+              }
+            }
+
+            var id = "#question" + index;
+            var qIndex = _.indexOf(incorrectQuestions, id);
+            if (answer.correct === false || !answer.correct) {
+              if (qIndex === -1) {
+                incorrectQuestions.push(id);
+              }
+            } else {
+              correctNum++;
+              if (qIndex !== -1) {
+                $(incorrectQuestions[qIndex]).removeClass("incorrect");
+                incorrectQuestions.splice(qIndex, 1);
+              }
+            }
+          };
+
+          $scope.evaluate = function () {
+            for (var i = 0; i < incorrectQuestions.length; i++) {
+              $(incorrectQuestions[i]).addClass("incorrect");
+            }
+            if (!incorrectQuestions[0] && correctNum >= $(".question").length) {
+              $scope.nextBlock();
+            } else {
+              $scope.err = {};
+              $scope.err.message = "Looks like you have some incorrect or incomplete answers.";
+            }
+          };
         },
 
         link: function postLink(scope, element, attrs) {}
