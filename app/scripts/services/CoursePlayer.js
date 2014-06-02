e56a465c427bb681b190d226db8680a152b1041b
Randall A. Gordon <randall@mindkitchenmedia.com>
2014-06-02 15:23:44 -0700

    Revert a/s/s/CoursePlayer

--- app/scripts/services/CoursePlayer.js
+++ app/scripts/services/CoursePlayer.js
@@ -154,6 +154,7 @@ angular.module("ettoPupil")
         var tmp = course.blocks[a];
         course.blocks[a] = course.blocks[b];
         course.blocks[b] = tmp;
+
         return true;
       };
 
