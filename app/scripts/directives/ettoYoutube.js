a7d6b7d15d30e3625d92c000b665ea7117e7489a
Nate <nate@interactivebalance.com>
2014-10-15 11:13:15 -0700

    niew and video urls

--- app/scripts/directives/ettoYoutube.js
+++ app/scripts/directives/ettoYoutube.js
@@ -11,7 +11,15 @@ angular.module("ettoPupil")
         },
         template: "<div class='video-container'><iframe width='100%' height='100%' src='{{ ytUrl }}' frameborder='0' allowfullscreen></iframe></div>",
         link: function (scope, element, attrs) {
-          scope.ytUrl = $sce.trustAsResourceUrl("//www.youtube.com/embed/" + scope.code);
+
+          var code = scope.code;
+
+          if (code.substring(0, 4) === "http") {
+            var parts = code.split('v=', 2);
+            code = parts[1];
+          }
+
+          scope.ytUrl = $sce.trustAsResourceUrl("//www.youtube.com/embed/" + code);
         }
 
       };
