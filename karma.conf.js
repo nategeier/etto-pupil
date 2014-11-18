d9f9dd348cf9892c1867f32c98eb5b357533e4b7
Nate <nate@interactivebalance.com>
2014-11-17 18:01:56 -0800

    ssl ready google maps

--- karma.conf.js
+++ karma.conf.js
@@ -21,6 +21,7 @@ module.exports = function (config) {
       "app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js",
       "app/bower_components/angular-touch/angular-touch.js",
       "app/bower_components/angular-leaflet-directive/dist/angular-leaflet-directive.js",
+      "app/bower_components/ngAutocomplete/src/ngAutocomplete.js",
 
       "app/bower_components/angular-mocks/angular-mocks.js",
       "app/bower_components/angular-loading-bar/src/loading-bar.js",
